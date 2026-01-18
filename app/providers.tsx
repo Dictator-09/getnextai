2bfcef9a3c2680a3360a65d22cc58698d80fdd70
pratyaksh saini <pratyaksh.saini.ug25@nsut.ac.in>
2026-01-18 22:54:13 +0530

    Chore: Removed redundant files - SmoothScroll, ScrollProgress, PageTransition, MagneticCursor

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,14 +1,11 @@
 "use client";
 
-import ScrollProgress from "@/components/ui/ScrollProgress";
 import ScrollToTop from "@/components/ui/ScrollToTop";
 import LazyFloatingParticles from "@/components/ui/LazyFloatingParticles";
 import { ToastProvider } from "@/components/ui/Toast";
 import WebVitalsReporter from "@/components/ui/WebVitalsReporter";
 import { CursorProvider } from "@/components/ui/CustomCursor";
-import SmoothScroll from "@/components/ui/SmoothScroll";
 import PreLoader from "@/components/ui/PreLoader";
-import PageTransition from "@/components/ui/PageTransition";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
     return (
@@ -16,14 +13,9 @@ export default function Providers({ children }: { children: React.ReactNode }) {
             <ToastProvider>
                 <PreLoader />
                 <WebVitalsReporter />
-                <ScrollProgress />
-                <SmoothScroll>
-                    <PageTransition>
-                        <LazyFloatingParticles />
-                        <ScrollToTop />
-                        {children}
-                    </PageTransition>
-                </SmoothScroll>
+                <LazyFloatingParticles />
+                <ScrollToTop />
+                {children}
             </ToastProvider>
         </CursorProvider>
     );
